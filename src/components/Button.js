b6b9eb66e6ab063bed32c7b750efb480cf69c088
Steve Bauman <steven_bauman@outlook.com>
2021-01-21 11:26:33 -0500

    Update Button.js

--- src/components/Button.js
+++ src/components/Button.js
@@ -3,8 +3,10 @@ import {  Button } from 'react-bootstrap'
 import styles from './Button.module.scss'
 
 export default function ControlButton({style}) {
-    return (<div>
-        <Button className={`m-2 ${styles.signup}`} style={style}>Sign up</Button>
-        <Button variant="none"  className={`m-2 ${styles.login}`} style={style}>Sign In</Button>
-    </div>)
+    return (
+        <div>
+            <Button className={`m-2 ${styles.signup}`} style={style}>Sign up</Button>
+            <Button variant="none" className={`m-2 ${styles.login}`} style={style}>Sign In</Button>
+        </div>
+    );
 }
